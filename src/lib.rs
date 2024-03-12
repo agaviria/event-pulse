780947a39376482e23399e1cd88dee14210c9215
Alejandro Gaviria <mr.gaviria@gmail.com>
2024-03-12 16:33:11 -0400

    first cargo lib commit.  Added time constants.

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+/// Days in a single week.
+#[allow(dead_code)]const DAYS_IN_WEEK: i64 = 7;
+/// Days ia a year. (Approximation)
+#[allow(dead_code)]const DAYS_IN_YEAR: i64 = 365;
+/// Days in a month. (Approximation)
+#[allow(dead_code)]const DAYS_IN_MONTH: i64 = 30;
+/// Number of seconds in a day.
+#[allow(dead_code)]const SECS_IN_DAY: i64 = 24 * 60 * 60;
+/// Number of seconds in a week.
+#[allow(dead_code)]const SECS_IN_WEEK: i64 = 7 * SECS_IN_DAY;
+/// Number of seconds in a bi-weekly period (two weeks).
+#[allow(dead_code)]const SECS_IN_BI_WEEKLY: i64 = 2 * SECS_IN_WEEK;
+/// Number of seconds in a month. (Approximation based on Gregorian month length)
+#[allow(dead_code)]const SECS_IN_MONTH: i64 = 30 * 24 * 60 * 60;
+/// Number of seconds in a quarter. (Approximation based on average, Gregorian quarter)
+#[allow(dead_code)]const SECS_IN_QUARTER: i64 = 3 * SECS_IN_MONTH;
