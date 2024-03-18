bf9748e55eb816bc98802753d0df95915af699a9
Alejandro Gaviria <mr.gaviria@gmail.com>
2024-03-18 14:49:07 -0400

    Added wrapper for StructsyError to test code, in specific error messages.

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,33 @@
+use std::fmt;
 use structsy::StructsyError;
 use thiserror::Error; // Importing the `Error` trait and derive macro from the `thiserror` crate
 
+// Define a wrapper struct around StructsyError so that we can create our own PartialEq trait.
+// PartialEq trait is important for our application, as it is required for testing purposes.
+#[derive(Debug)]
+pub struct StructsyErrWrapper(pub StructsyError);
+
+impl std::error::Error for StructsyErrWrapper {}
+
+impl PartialEq for StructsyErrWrapper {
+    fn eq(&self, other: &Self) -> bool {
+        // Implement PartialEq based on our testing requirements
+        // For simplicity, let's assume equality if the error messages are the same.
+        self.0.to_string() == other.0.to_string()
+    }
+}
+
+impl fmt::Display for StructsyErrWrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 /// Defines application logic error types.
-#[derive(Debug, Error)]
+#[derive(Debug, PartialEq, Error)]
 pub enum AppError {
     #[error("Structsy error: {0}")]
-    StructsyError(#[from] StructsyError),
+    StructsyError(#[from] StructsyErrWrapper),
 
     #[error("Invalid string format: {0}")]
     InvalidInputString(String),
